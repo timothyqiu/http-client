d48075d945dcdb7c4ce954327c20c37e931edaf2
Haoyu Qiu <timothyqiu32@gmail.com>
2020-02-20 13:08:51 +0800

    Refactors with exception

--- openssl-http-client/src/client.cpp
+++ openssl-http-client/src/client.cpp
@@ -1,5 +1,8 @@
+#include <algorithm>
 #include <cstdio>
 #include <cstdlib>
+#include <exception>
+#include <map>
 #include <string>
 #include <vector>
 
@@ -8,18 +11,48 @@
 
 #include "scope_guard.hpp"
 
-int main()
+class OpenSslError : public std::runtime_error {
+public:
+    OpenSslError(char const *message)
+        : std::runtime_error{message}
+    {
+        // TODO: store instead of print
+        ERR_print_errors_fp(stderr);
+    }
+};
+
+// TODO: get rid of the ugly bool
+static bool receiveData(BIO *bio, std::vector<char>& buffer, size_t& received)
 {
+    size_t const minimumAvailable = 128;
+    while (buffer.size() - received < minimumAvailable) {
+        buffer.resize(std::max(buffer.size() * 2, buffer.size() + minimumAvailable));
+    }
+
+    int const n = BIO_read(bio, buffer.data() + received, buffer.size() - received);
+    if (n < 1) {
+        if (BIO_should_retry(bio)) {
+            return receiveData(bio, buffer, received);
+        }
+        if (n == 0) {  // TODO: Is this reasonable?
+            return false;
+        }
+        throw OpenSslError{"error receiving response"};
+    }
+    received += n;
+    return true;
+}
+
+int main()
+try {
     BIO *bio = BIO_new_connect("httpbin.org:80");
     if (bio == nullptr) {
-        ERR_print_errors_fp(stderr);
-        return EXIT_FAILURE;
+        throw OpenSslError{"error BIO_new_connect"};
     }
     SCOPE_EXIT([&]{ BIO_free_all(bio); });
 
     if (BIO_do_connect(bio) < 1) {
-        ERR_print_errors_fp(stderr);
-        return EXIT_FAILURE;
+        throw OpenSslError{"error BIO_do_connect"};
     }
 
     std::string const message{"GET /get HTTP/1.0\r\n\r\n"};
@@ -28,36 +61,24 @@ int main()
     while (sent < message.size()) {
         int const n = BIO_write(bio, message.data() + sent, message.size() - sent);
         if (n < 1) {
-            std::fprintf(stderr, "Error sending request\n");
-            ERR_print_errors_fp(stderr);
-            return EXIT_FAILURE;
+            throw OpenSslError{"error sending request"};
         }
         sent += n;
     }
 
-    size_t const initialBufferSize = 128;
-    std::vector<char> buffer(initialBufferSize);
+    // TODO: make a dedicated buffer class
+    std::vector<char> buffer;
     size_t received = 0;
-    while (true) {
-        int const n = BIO_read(bio, buffer.data() + received, buffer.size() - received);
-        if (n < 1) {
-            if (BIO_should_retry(bio)) {
-                continue;
-            }
-            if (n == 0) {
-                break;
-            }
-            std::fprintf(stderr, "Error receiving response\n");
-            ERR_print_errors_fp(stderr);
-            return EXIT_FAILURE;
-        }
-        received += n;
 
-        if (buffer.size() - received == 0) {
-            buffer.resize(buffer.size() * 2);
-        }
+    // TODO: check content-length
+    while (receiveData(bio, buffer, received)) {
     }
+    buffer.resize(received);
 
     std::string const response{std::begin(buffer), std::end(buffer)};
-    std::printf("%zu: %s\n", received, response.c_str());
+    std::printf("%s\n", response.c_str());
+}
+catch (std::exception const& e) {
+    std::fprintf(stderr, "Exception: %s\n", e.what());
+    return EXIT_FAILURE;
 }
